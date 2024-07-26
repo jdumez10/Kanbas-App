fe9d8e16bc02b10a4ce134c5c71fed13b2304180
jdumez10 <jd.dumez10@uniandes.edu.co>
2024-07-26 15:28:47 -0400

    A6

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
 import bodyParser from "body-parser";
 import cors from "cors";
+import session from "express-session";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
-import AssignmentRoutes from "./Kanbas/Assignments/routes.js"; 
+import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import UserRoutes from "./Users/routes.js";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: process.env.NETLIFY_URL || "http://localhost:3000",
+}));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kanbas",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+
 CourseRoutes(app);
 ModuleRoutes(app);
-AssignmentRoutes(app); 
+AssignmentRoutes(app);
+UserRoutes(app);
 Lab5(app);
 Hello(app);
 
